2907b32827f5b89fbbb7924f3d8aa5dad99be0cf
Darren Soukup <soukman@gmail.com>
2022-04-02 07:43:00 -0600

    updates

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
+// module.exports = {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
+//   setupFiles: ["dotenv/config"],
+// };
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  setupFiles: ["dotenv/config"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  // testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testPathIgnorePatterns: ["/dist/", "/node_modules/"],
 };
